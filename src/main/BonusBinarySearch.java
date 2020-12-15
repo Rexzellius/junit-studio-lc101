35aba1c06a97de15a8a1efecb7058803b1cb5ffa
Rexzellius <katlynmmills@gmail.com>
2020-12-14 20:27:40 -0600

    Added bonus mission.

--- src/main/BonusBinarySearch.java
+++ src/main/BonusBinarySearch.java
@@ -2,6 +2,11 @@ package main;
 
 public class BonusBinarySearch {
 
+    public static void main(String[] args){
+        int[] thisArray = new int[] {0,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        binarySearch(thisArray, 1);
+    }
+
     /**
      * A binary search implementation for integer arrays.
      *
@@ -11,12 +16,12 @@ public class BonusBinarySearch {
      * @param n - number to search for
      * @return index of search item if it's found, -1 if not found
      */
-
     public static int binarySearch(int[] sortedNumbers, int n) {
         int right = sortedNumbers.length - 1;
         int left = 0;
-        while (right >= left) {
-            int mid = left + ((right - left) / 2);
+        int mid = left + ((right - left) / 2);
+        while (mid > left) {
+            mid = left + ((right - left) / 2);
             if (sortedNumbers[mid] > n) {
                 right = mid;
             } else if (sortedNumbers[mid] < n) {
@@ -25,7 +30,7 @@ public class BonusBinarySearch {
                 return mid;
             }
         }
+        System.out.println(-1);
         return -1;
     }
-
 }
